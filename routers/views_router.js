97a5d4d19df47a854085b62daf0de336cf085da7
shohruzbek <shohruzbekochilov007@gmail.com>
2022-08-11 06:10:21 +0500

    basic page for admins

--- routers/views_router.js
+++ routers/views_router.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 
 router.get('/', async (req,res) => {
     return res.render('basic', {
-
+        user: {}
     });
 });
 
@@ -16,7 +16,7 @@ router.get('/reg', async (req,res) => {
 });
 
 router.post('/new_reg', async (req,res) => {
-    req.body.role = '3';
+    req.body.role = 1;
     let user = new User(_.pick(req.body, ['name', 'email', 'password', 'phone','role']));
     const seve_user = await user.save();
     if(seve_user){
@@ -28,15 +28,49 @@ router.post('/new_reg', async (req,res) => {
             // signed: true
         })
         return res.render('basic', {
-
+            user: _.pick(user, ['name', 'email', 'password', 'phone','role'])
         });
     }
 });
 
 router.get('/login', async (req,res) => {
     return res.render('login', {
-
+        
     });
+});
+
+router.post('/login', async (req,res) => {
+    let user = await User.findOne(req.body)
+    console.log(user)
+    if(!user){
+        return res.render('login', {
+        
+        });
+    } else {
+        const token = user.generateAuthToken();
+        res.cookie("token", token, {
+            httpOnly: true,
+            // secure: true,
+            // maxAge: 1000000,
+            // signed: true
+        })
+        if(user.role == 3){
+            return res.render('basic', {
+                user: _.pick(user, ['name', 'email', 'password', 'phone','role'])
+            });
+        }
+        if(user.role == 2){
+            return res.render('basic_admin', {
+                user: _.pick(user, ['name', 'email', 'password', 'phone','role'])
+            });
+        }
+        if(user.role == 1){
+            return res.render('basic_admin', {
+                user: _.pick(user, ['name', 'email', 'password', 'phone','role'])
+            });
+        }
+    }
+    
 });
 
 
